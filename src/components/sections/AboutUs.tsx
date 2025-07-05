3f48cf055ab5bbffd3e70988cebfaedf3e92f600
Md. Rafeul Islam Rafe <rafe.19.dev@gmail.com>
2025-07-05 20:42:33 +0600

    Updated and fixed for deployment now

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -22,11 +22,11 @@ export const AboutUs = () => {
           <Title>Overview of Our AI Capabilities</Title>
           <Paragraph>
             <br></br>
-
-            We’re dedicated to empowering businesses through transformative AI solutions. 
-            Backed by cutting-edge research and innovative algorithms, our expert team delivers a platform designed to adapt seamlessly to your unique challenges. 
-            Partner with us and take the lead in your industry's digital revolution.
-
+            We’re dedicated to empowering businesses through transformative AI
+            solutions. Backed by cutting-edge research and innovative algorithms,
+            our expert team delivers a platform designed to adapt seamlessly to
+            your unique challenges. Partner with us and take the lead in your
+            industry's digital revolution.
           </Paragraph>
 
           <div className="pt-8 grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-3xl">
@@ -34,13 +34,46 @@ export const AboutUs = () => {
               title="What Drives Us"
               description="Empowering positive change through responsible and ethical AI for humanity and Earth."
             >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-land-plot-icon lucide-land-plot"><path d="m12 8 6-3-6-3v10"/><path d="m8 11.99-5.5 3.14a1 1 0 0 0 0 1.74l8.5 4.86a2 2 0 0 0 2 0l8.5-4.86a1 1 0 0 0 0-1.74L16 12"/><path d="m6.49 12.85 11.02 6.3"/><path d="M17.51 12.85 6.5 19.15"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-land-plot-icon lucide-land-plot"
+              >
+                <path d="m12 8 6-3-6-3v10" />
+                <path d="m8 11.99-5.5 3.14a1 1 0 0 0 0 1.74l8.5 4.86a2 2 0 0 0 2 0l8.5-4.86a1 1 0 0 0 0-1.74L16 12" />
+                <path d="m6.49 12.85 11.02 6.3" />
+                <path d="M17.51 12.85 6.5 19.15" />
+              </svg>
             </Info>
             <Info
               title="Shaping the Future"
               description="Leading the way in business transformation through intelligent innovation."
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrows-up-from-line-icon lucide-arrows-up-from-line"><path d="m4 6 3-3 3 3"/><path d="M7 17V3"/><path d="m14 6 3-3 3 3"/><path d="M17 17V3"/><path d="M4 21h16"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrows-up-from-line-icon lucide-arrows-up-from-line"
+              >
+                <path d="m4 6 3-3 3 3" />
+                <path d="M7 17V3" />
+                <path d="m14 6 3-3 3 3" />
+                <path d="M17 17V3" />
+                <path d="M4 21h16" />
+              </svg>
             </Info>
           </div>
         </div>
--- src/utils/services-data.tsx
+++ src/utils/services-data.tsx
@@ -35,14 +35,50 @@ export const services = [
     title: "Immediate Performance Metrics",
     description: "Make smarter decisions with live, actionable intelligence.",
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-combined-icon lucide-chart-no-axes-combined"><path d="M12 16v5"/><path d="M16 14v7"/><path d="M20 10v11"/><path d="m22 3-8.646 8.646a.5.5 0 0 1-.708 0L9.354 8.354a.5.5 0 0 0-.707 0L2 15"/><path d="M4 18v3"/><path d="M8 14v7"/></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-chart-no-axes-combined-icon lucide-chart-no-axes-combined"
+      >
+        <path d="M12 16v5" />
+        <path d="M16 14v7" />
+        <path d="M20 10v11" />
+        <path d="m22 3-8.646 8.646a.5.5 0 0 1-.708 0L9.354 8.354a.5.5 0 0 0-.707 0L2 15" />
+        <path d="M4 18v3" />
+        <path d="M8 14v7" />
+      </svg>
     ),
   },
   {
     title: "Intelligent Trend Prediction",
     description: " Identify opportunities and prevent issues before they arise.",
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-combine-icon lucide-combine"><path d="M10 18H5a3 3 0 0 1-3-3v-1"/><path d="M14 2a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2"/><path d="M20 2a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2"/><path d="m7 21 3-3-3-3"/><rect x="14" y="14" width="8" height="8" rx="2"/><rect x="2" y="2" width="8" height="8" rx="2"/></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-combine-icon lucide-combine"
+      >
+        <path d="M10 18H5a3 3 0 0 1-3-3v-1" />
+        <path d="M14 2a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2" />
+        <path d="M20 2a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2" />
+        <path d="m7 21 3-3-3-3" />
+        <rect x="14" y="14" width="8" height="8" rx="2" />
+        <rect x="2" y="2" width="8" height="8" rx="2" />
+      </svg>
     ),
   },
 ];
